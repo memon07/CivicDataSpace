2ed1069142dfd077bfdbb6b05303ae144735a3cf
shoyebec <memonshoyeb26@gmail.com>
2025-07-14 00:44:25 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 import { env } from "process";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [env.REPLIT_DOMAINS.split(",")[0]],
+  allowedDevOrigins: ["http://localhost:3000"],
+  // [env.REPLIT_DOMAINS.split(",")[0]],
 };
 
 module.exports = nextConfig;
